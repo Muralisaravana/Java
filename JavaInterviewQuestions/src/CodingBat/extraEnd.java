7c0119494403ae55b8ad63de2a3bb601d4ece6b9
MuraliSaravana <muralisaravanan.it@gmail.com>
2026-01-08 23:29:41 +0530

    Trim last two digits and repeat thrice

---
+++ JavaInterviewQuestions/src/CodingBat/extraEnd.java
@@ -0,0 +1,19 @@
+/*Given a string, return a new string made of 3 copies of the last 2 chars of the original string. 
+ * The string length will be at least 2
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"*/
+package CodingBat;
+
+public class extraEnd {
+
+	public static void main(String[] args) {
+		System.out.println(extraEnd("Hello"));
+		System.out.println(extraEnd("ab"));
+		System.out.println(extraEnd("Hi"));
+	}
+    public static String extraEnd(String str) {
+    	String s = str.substring(str.length()-2);
+    	return s+s+s;
+    }
+}
