41b375eca1579893bab086591aa7332380fdd137
MuraliSaravana <muralisaravanan.it@gmail.com>
2026-01-24 15:44:49 +0530

    NTwice

---
+++ JavaInterviewQuestions/src/CodingBat/NTwice.java
@@ -0,0 +1,22 @@
+/*Given a string and an int n, return a string made of the first and last n chars from the string. 
+ * The string length will be at least n.
+nTwice("Hello", 2) → "Helo"
+nTwice("Chocolate", 3) → "Choate"
+nTwice("Chocolate", 1) → "Ce"*/
+package CodingBat;
+
+public class NTwice {
+
+	public static void main(String[] args) {
+		System.out.println(nTwice("Hello", 2));
+		System.out.println(nTwice("Chocolate", 3));
+		System.out.println(nTwice("Chocolate", 1));
+	}
+
+	private static String nTwice(String str, int n) {
+		String first = str.substring(0,n);
+		String last = str.substring(str.length()-n);
+		return first + last ;
+	}
+
+}
